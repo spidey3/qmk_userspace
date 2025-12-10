c36c0ea325291af9e29e0ddd0ab96224fb04c99e
Joshua Diamond <josh@windowoffire.com>
2025-12-09 22:20:55 -0500

    Disable RBGLIGHT (underglow) on kbd67

--- keyboards/kbdfans/kbd67/rev2/keymaps/spidey3/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/keymaps/spidey3/rules.mk
@@ -5,6 +5,7 @@ MOUSEKEY_ENABLE = no	# Mouse keys
 UNICODEMAP_ENABLE = yes
 VELOCIKEY_ENABLE = yes
 GRAVE_ESC_ENABLE = yes
+RGBLIGHT_ENABLE = no
 
 # The following disabled to save space
 SPACE_CADET_ENABLE = no
