cf072659e3a48e8ecaa8f16070526fdcf1cbab45
Joshua Diamond <josh@windowoffire.com>
2025-12-14 23:06:22 -0500

    Layer tap for ikki68

--- keyboards/wuque/ikki68_aurora/keymaps/spidey3/keymap.c
+++ keyboards/wuque/ikki68_aurora/keymaps/spidey3/keymap.c
@@ -3,6 +3,8 @@
 
 #include "spidey3.h"
 
+#define FN_MENU LT(_FN,KC_APP)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base
     [_BASE] = LAYOUT_68_ansi(
@@ -10,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,    KC_RBRC,   KC_BSLS,       KC_END,    KC_PGDN,
         KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,           KC_ENT,
         KC_LSFT,              KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,           KC_RSFT,           KC_UP,
-        KC_LCTL,   KC_LGUI,   KC_LALT,                                    KC_SPC,                                     KC_RALT,   MO(_FN),    KC_RCTL,       KC_LEFT,   KC_DOWN,   KC_RGHT
+        KC_LCTL,   KC_LGUI,   KC_LALT,                                    KC_SPC,                                     KC_RALT,   FN_MENU,    KC_RCTL,       KC_LEFT,   KC_DOWN,   KC_RGHT
     ),
     // Numpad
     [_NUMPAD] = LAYOUT_68_ansi(
