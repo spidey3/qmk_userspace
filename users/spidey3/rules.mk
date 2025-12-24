20a2b042a58377246ee4adfc2d09ceab6e459655
Joshua Diamond <josh@windowoffire.com>
2025-12-24 16:02:42 -0500

    trim firmware to fit (don't need Magic anymore)

--- users/spidey3/rules.mk
+++ users/spidey3/rules.mk
@@ -1,4 +1,5 @@
 BOOTMAGIC_ENABLE = yes    # Enable Bootmagic Lite
+MAGIC_ENABLE = no         # Disable the old "Magic"
 CONSOLE_ENABLE = yes      # Console for debug
 OS_DETECTION_ENABLE = yes # Experimental OS detection
 BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
