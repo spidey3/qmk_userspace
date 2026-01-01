fdd4f898b47ba324467038b7469066760c48012b
Joshua Diamond <josh@windowoffire.com>
2025-12-31 19:37:01 -0500

    Slight improvement to wuque/ikki68_aurora lighting plan

--- keyboards/wuque/ikki68_aurora/keymaps/spidey3/config.h
+++ keyboards/wuque/ikki68_aurora/keymaps/spidey3/config.h
@@ -10,19 +10,23 @@
 #define MAX_LAYER 4
 
 // LED map
-// 
+//
 //            back
 //   7   6   5   4   3   2   1
 //   8                       0
 //   9  10  11  12  13  14  15
 //            front
-//   
+//
 // LEDs 16-19 illuminate the top logo
+//
+//                       16 17
+//                       18 19
+//
 
-#define LRGB_CORNER_FL(color) { 8, 2, color }
-#define LRGB_CORNER_FR(color) { 0, 1, color }, { 15, 1, color }
-#define LRGB_CORNER_BR(color) { 1, 1, color }
-#define LRGB_CORNER_BL(color) { 7, 1, color }
+#define LRGB_CORNER_FL(color) { 8, 2, color }, { 16, 1, color }
+#define LRGB_CORNER_FR(color) { 0, 1, color }, { 15, 1, color }, { 17, 1, color }
+#define LRGB_CORNER_BR(color) { 1, 1, color }, { 19, 1, color }
+#define LRGB_CORNER_BL(color) { 7, 1, color }, { 18, 1, color }
 #define LRGB_CORNERS(color) { 0, 2, color }, { 7, 3, color }, { 15, 1, color }
 #define LRGB_FRONT(inset, color) { 9+inset, 15-inset, color }
 #define LRGB_BACK(inset, color) { 1+inset, 7-inset, color }
